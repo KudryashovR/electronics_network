c84f617900463ff8a710025d10c25021737e3c05
Roman Kudryashov <romkud55@gmail.com>
2024-10-30 00:10:20 +0300

    ADD: Permissions for staff

--- network/views.py
+++ network/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, filters
 
 from network.models import NetworkNode, Product
+from network.permissions import IsStaff
 from network.serializers import NetworkNodeSerializer, ProductSerializer
 
 
@@ -9,8 +10,10 @@ class NetworkNodeViewSet(viewsets.ModelViewSet):
     serializer_class = NetworkNodeSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['country']
+    permission_classes = [IsStaff]
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsStaff]
