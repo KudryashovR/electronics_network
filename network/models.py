8ec78e26417b44cd516844f090356ca36c824ebe
Roman Kudryashov <romkud55@gmail.com>
2024-10-29 22:37:32 +0300

    ADD: Network node model

--- network/models.py
+++ network/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class NetworkNode(models.Model):
+    LEVEL_CHOICES = (
+        (0, 'Завод'),
+        (1, 'Розничная сеть'),
+        (2, 'Индивидуальный предприниматель'),
+    )
+
+    name = models.CharField(max_length=255, verbose_name='Название')
+    level = models.IntegerField(choices=LEVEL_CHOICES, verbose_name='Уровень иерархии')
+    supplier = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='clients',
+                                 verbose_name='Поставщик')
+
+    email = models.EmailField(verbose_name='email')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    street = models.CharField(max_length=100, verbose_name='Улица')
+    house_number = models.CharField(max_length=10, verbose_name='Номер дома')
+
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Время создания')
+
+    debt = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Задолжность')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Звено сети'
+        verbose_name_plural = 'Звенья сети'
