a12a010e3c585dbb01b9ac9381cb5c3870eb3af1
Roman Kudryashov <romkud55@gmail.com>
2024-10-29 23:48:15 +0300

    ADD: Network node viewset

--- network/views.py
+++ network/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from network.models import NetworkNode
+from network.serializers import NetworkNodeSerializer
+
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['country']
