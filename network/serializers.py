a4ab870a9daa5275c36e548e5377677384000b3e
Roman Kudryashov <romkud55@gmail.com>
2024-10-29 23:44:54 +0300

    ADD: Product serializer

--- network/serializers.py
+++ network/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from network.models import NetworkNode
+from network.models import NetworkNode, Product
 
 
 class NetworkNodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = NetworkNode
         exclude = ['debt']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
