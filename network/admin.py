16b5051dd3862a06bd67e15025fc48ef8ed942f0
Roman Kudryashov <romkud55@gmail.com>
2024-10-29 23:08:07 +0300

    ADD: Network node to admin panel

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import NetworkNode
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'level', 'get_full_address', 'debt', 'created_at']
+    search_fields = ['name', 'city']
+    list_filter = ['city']
+
+    get_full_address = lambda self, obj: obj.get_full_address()
+    get_full_address.short_description = 'Полный адрес'
+
+    actions = ['clear_debt']
+
+    @admin.action(description='Очистить задолжность')
+    def crear_debt(self, request, queryset):
+        queryset.update(debt=0.00)
