237ff8c835ceaa6dcc817791131dc6c918a00002
Roman Kudryashov <romkud55@gmail.com>
2024-11-01 00:01:26 +0300

    ADD: Command populate_products tests

--- network/tests/tests_commands.py
+++ network/tests/tests_commands.py
@@ -1,7 +1,10 @@
+from datetime import date, timedelta
+from unittest.mock import patch
+
 from django.core.management import call_command
 from django.test import TestCase
 
-from network.models import NetworkNode
+from network.models import NetworkNode, Product
 
 
 class CommandTestCase(TestCase):
@@ -36,3 +39,37 @@ class CommandTestCase(TestCase):
             self.assertIsNotNone(node.city)
             self.assertIsNotNone(node.street)
             self.assertIsNotNone(node.house_number)
+
+    def test_product_creation(self):
+        self.assertEqual(NetworkNode.objects.count(), 0)
+
+        self.assertEqual(Product.objects.count(), 0)
+
+        call_command('populate_network_nodes')
+        call_command('populate_products')
+
+        self.assertEqual(Product.objects.count(), 100)
+
+        products = Product.objects.all()
+        for product in products:
+            self.assertIsNotNone(product.name)
+            self.assertIsNotNone(product.model)
+            self.assertIsNotNone(product.release_date)
+            self.assertIsNotNone(product.supplier)
+
+        today = date.today()
+        five_years_ago = date.today() - timedelta(days=365 * 5)
+        for product in products:
+            self.assertLessEqual(product.release_date, today)
+            self.assertGreaterEqual(product.release_date, five_years_ago)
+
+    @patch('network.models.NetworkNode.objects.all')
+    def test_no_network_nodes_message_and_return(self, mock_objects_all):
+        mock_objects_all.return_value = []
+
+        with self.assertLogs(level='ERROR') as logs:
+            call_command('populate_products')
+
+        self.assertIn("Звенья сети недоступны. Пожалуйста, сначала заполните их.", logs.output[0])
+
+        self.assertEqual(Product.objects.count(), 0)
